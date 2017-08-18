7303cc318b66d1c00f377db96f83cddaf1c304df
isabellarner <larner.isabel@gmail.com>
2017-08-18 12:00:21 +0100

    adds passing ReturnsDraw and RockBeatsScissors tests

--- src/game.php
+++ src/game.php
@@ -2,4 +2,19 @@
 
 class Rock {}
 
+class Paper {}
+
+class Scissors {}
+
+class Game {
+  public function evaluates($choice1, $choice2) {
+    if (get_class($choice1) === get_class($choice2)) {
+      return "Draw";
+    }
+    else {
+      return $choice1;
+      }
+    }
+  }
+
 ?>
--- tests/gameTest.php
+++ tests/gameTest.php
@@ -2,10 +2,29 @@
 
 require './src/game.php';
 
+// $this->someAssertion($actual, $expected);
+
 class GameTest extends PHPUnit_Framework_TestCase {
-  public function testCheckType(){
+  public function testCheckType() {
     $rock = new Rock();
     $this->assertEquals(get_class($rock), "Rock");
+    $paper = new Paper();
+    $this->assertEquals(get_class($paper), "Paper");
+    $scissors = new Scissors();
+    $this->assertEquals(get_class($scissors), "Scissors");
+  }
+
+  public function testReturnsDraw() {
+    $game = new Game();
+    $rock = new Rock();
+    $this->assertEquals($game->evaluates($rock, $rock), "Draw");
+  }
+
+  public function testRockBeatsScissors() {
+    $game = new Game();
+    $rock = new Rock();
+    $scissors = new Scissors();
+    $this->assertEquals($game->evaluates($rock, $scissors), $rock);
   }
 }
 
